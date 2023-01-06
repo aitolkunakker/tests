749c89f00444c1b500f1bbae72257113ec356691
aitolkun <aitolkuns52@gmail.com>
2023-01-06 12:36:06 +0600

    test

---
+++ account/tests.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework_simplejwt.views import TokenObtainPairView  
+
+from .views import RegisterAPIView
+from .models import User
+
+
+class Auth(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        User.objects.create_user(
+            email='test@gmail.com',
+            password='12345678'
+
+
+        )
+    def test_register(self):
+        data={
+            'email':'new_user@gmail.com',
+            'password':'12345678',
+            'password_confirm':'12345678'
+            }
+
+        request = self.factory.post('/account/register/', data, format='json')
+        view=RegisterAPIView.as_view()
+        response=view(request)
+
+        assert response.status_code==201
+
+        # print(response)
+        
+        assert User.objects.filter(email=data['email']).exists()
+        print(User.objects.all())
+    
+    def test_login(self):
+        data={
+            'email':'test@gmail.com',
+            'password':'12345678'
+            }
+        request = self.factory.post('/account/token/', data,format='json')
+        view = TokenObtainPairView.as_view()
+        response = view(request)
+        
+    
+        assert response.status_code ==200
+        assert 'access'  in response.data
+        assert 'refresh'  in response.data
---
+++ main/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        exclude = ('author',)
+    
+    def validate(self, attrs):
+        super().validate(attrs)
+        attrs['author'] = self.context.get('request').user
+        return attrs
+    
+    def to_representation(self, instance:Post):
+        rep = super().to_representation(instance)
+        rep['author'] = instance.author.id
+        return rep
